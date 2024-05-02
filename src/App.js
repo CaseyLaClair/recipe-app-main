05584ff6654a39af5295d314965dda68ffebbb91
Casey LaClair <casey.a.laclair@gmail.com>
2024-05-02 13:21:59 -0400

    Init Commit
    Initial commit of recipe-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React, { useEffect, useState } from 'react'; 
+import RecipeCard from './RecipeCard'; 
+  
+const App = () => { 
+    const APP_ID = '1debf8e5'; 
+    const APP_KEY = '24a5fafb50d8565500ba39e12703cd2a'; 
+    const [food_recipes, setfood_recipes] = useState([]); 
+    const [search_recipe, setSearch_recipe] = useState(''); 
+    const [search_query, setSearch_Query] = useState('chicken'); 
+  
+    useEffect(() => { 
+        getRecipesFunction(); 
+    }, [search_query]); 
+  
+    const getRecipesFunction = async () => { 
+        const response = await fetch( 
+`https://api.edamam.com/search?q=${search_query}&app_id=${APP_ID}&app_key=${APP_KEY}` 
+        ); 
+        const data = await response.json(); 
+        setfood_recipes(data.hits); 
+    }; 
+  
+    const updateSearchFunction = (e) => { 
+        setSearch_recipe(e.target.value); 
+    }; 
+  
+    const getSearchFunction = (e) => { 
+        e.preventDefault(); 
+        setSearch_Query(search_recipe); 
+        setSearch_recipe(''); 
+    }; 
+  
+    return ( 
+        <div className="bg-green-50 p-6 min-h-screen font-sans"> 
+            <header className="bg-green-500 rounded-2xl shadow-md py-4 text-white"> 
+                <div className="container mx-auto text-center"> 
+                    <h1 className="text-3xl sm:text-4xl  
+                                   md:text-5xl lg:text-6xl 
+                                   font-extrabold tracking-tight"> 
+                        <span className="block">Basil's Recipe Finder</span> 
+                    </h1> 
+                </div> 
+            </header> 
+            <div className="container mx-auto mt-8 p-4  
+                            sm:px-6 lg:px-8"> 
+                <form 
+                    onSubmit={getSearchFunction} 
+                    className="bg-white p-4 sm:p-6  
+                               lg:p-8 rounded-lg shadow-md  
+                               flex flex-col sm:flex-row items-center  
+                               justify-center space-y-4 sm:space-y-0  
+                               sm:space-x-4"
+                > 
+                    <div className="relative justify-center flex-grow 
+                                    w-full sm:w-1/2"> 
+                        <input 
+                            type="text"
+                            name="search"
+                            value={search_recipe} 
+                            onChange={updateSearchFunction} 
+                            placeholder="Search for recipes..."
+                            className="w-full py-3 px-4 bg-gray-100  
+                                       border border-green-300 focus:ring-green-500  
+                                       focus:border-green-500 rounded-full  
+                                       text-gray-700 outline-none transition-colors  
+                                       duration-200 ease-in-out focus:ring-2  
+                                       focus:ring-green-900 focus:bg-transparent  
+                                       focus:shadow-md"
+                        /> 
+                    </div> 
+                    <button 
+                        type="submit"
+                        className="bg-green-500 hover:bg-green-600 focus:ring-2  
+                        focus:ring-green-900 text-white font-semibold py-3 px-6  
+                        rounded-full transform hover:scale-105 transition-transform  
+                        focus:outline-none focus:ring-offset-2  
+                        focus:ring-offset-green-700"
+                    > 
+                        Search Recipe 
+                    </button> 
+                </form> 
+            </div> 
+  
+            <div className="container mx-auto mt-8 p-4 sm:px-6 lg:px-8"> 
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2  
+                lg:grid-cols-3 gap-3"> 
+                    {food_recipes.map((recipe) => ( 
+                        <RecipeCard key={recipe.recipe.label} recipe={recipe.recipe} /> 
+                    ))} 
+                </div> 
+            </div> 
+        </div> 
+    ); 
+}; 
+  
+export default App; 
